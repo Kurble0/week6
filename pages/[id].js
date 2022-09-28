58bb56d1b9a1c53c73d4757b9ce17d8f6ddf08e5
Charles Wiltermood <cwiltermood@gmail.com>
2022-09-27 17:39:52 -0700

    another commit

--- pages/[id].js
+++ pages/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
+import Link from 'next/link';
 
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
@@ -22,18 +23,35 @@ export async function getStaticPaths() {
 
 export default function Entry({ itemData }) {
   return (
-     <Layout>
+    <Layout>
+      {/* render details about one entity in persons.json saved in itemData */}
       <article className="card col-6">
+        <h2>Person Detail</h2>
         <div className="card-body">
           <h5 className="card-title">{itemData.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
           <p className="card-text">{itemData.birthdate}</p>
-          <p className="card-text"> Allergies- {itemData.allergies}</p>
-           <p className="card-text"> Medical- {itemData.medical}</p>
           <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
         </div>
       </article>
-       </Layout>
-   
+      {/* render details about all other entities in persons.json related to id */}
+      <div className="list-group col-6">
+        {/* check for existence of itemData.related property */}
+        {itemData.related ? 
+          <h2>Related Persons</h2> : null
+        }
+        {itemData.related ? 
+          itemData.related.map(
+            ({ id, name }) => (
+              <Link key={id} href={`/${id}`}>
+                <a className="list-group-item list-group-item-action">{name}</a>
+              </Link>
+            )
+          )
+          : null
+        }
+        {/* using expression ? ... : null */}
+      </div>
+    </Layout>
   );
 }
